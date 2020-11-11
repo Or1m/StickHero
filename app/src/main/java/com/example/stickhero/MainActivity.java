bd36881b93c6546211f2504bfca9456dc8c18ca5
Your Name <you@example.com>
2020-11-12 00:02:43 +0100

    Urobene hlavne menu bez funkcionality

--- app/src/main/java/com/example/stickhero/MainActivity.java
+++ app/src/main/java/com/example/stickhero/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.stickhero;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
     }
 }
